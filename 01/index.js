d7828c81e8abcab78b895a109297742ebaac3c3b
dryego barbosa <dryego.lisboa@gmail.com>
2022-10-04 08:11:38 -0300

    ATENÇÃO

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,14 @@
-const comentario = "Esse COVID é muito perigoso!";
+const comentario = "Esse covid é muito perigoso!";
+
+const conversaoMaiuscula = comentario.toUpperCase();
+const localizadorCovid = comentario.indexOf("COVID");
+const localizadorPandemia = comentario.indexOf("PANDEMIA");
+
+if (localizadorCovid === -1 || localizadorPandemia === -1) {
+    console.log("Comentário autorizado");
+} else {
+    console.log("Comentário bloqueado por conter palavras proibidas");
+}
+
+// const validacao = localizadorCovid === false || localizadorPandemia === false ? "Comentário autorizado" : "Comentário bloqueado por conter palavras proibidas";
+// console.log(validacao);
