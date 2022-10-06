d923a24cba371d1f0aedd19f790c422bc4677fc6
dryego barbosa <dryego.lisboa@gmail.com>
2022-10-06 09:23:26 -0300

    Resolução, formatador CPF/CNPJ.

--- 02/index.js
+++ 02/index.js
@@ -2,21 +2,21 @@ const cpf = "12345678900";
 const cnpj = "12345678900000";
 
 function formatadorCPF(cpf) {
+    //cpf = cpf.replace(/[^\d]/g, "");
     if (cpf.length === 11) {
-        cpf = cpf.replace(/[^\d]/g, "");
         return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
     } else {
         console.log("CPF invalido.")
     }
 }
 function formatadorCNPJ(cnpj) {
+    //cnpj = cnpj.replace(/[^\d]/g, "");
     if (cnpj.length === 14) {
-        cnpj = cnpj.replace(/[^\d]/g, "");
         return cnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
     } else {
         console.log("CNPJ invalido.")
-
     }
 }
 
 console.log(formatadorCNPJ(cnpj));
+console.log(formatadorCPF(cpf));
