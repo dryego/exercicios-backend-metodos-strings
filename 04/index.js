8e25b1fa914b3eb16f1488beec002baba2bc3b5a
dryego barbosa <dryego.lisboa@gmail.com>
2022-10-06 23:21:37 -0300

    Resolução, funcão formatação de dados.

--- 04/index.js
+++ 04/index.js
@@ -1,3 +1,23 @@
 let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
+
+function formatador(identificador, nome, email) {
+    let identFormatado = "";
+    identFormatado = identificador.padStart(6, "0")
+    console.log(identFormatado);
+
+    const arrayNome = nome.split(" ");
+    let nomeFormatado = "";
+    for (let letra of arrayNome) {
+        let primeiraLetra = letra.slice(0, 1);
+        let restoNome = letra.slice(1);
+
+        nomeFormatado += (primeiraLetra.toUpperCase() + restoNome + " ");
+    }
+    console.log(nomeFormatado);
+
+    console.log(email.trim());
+}
+
+formatador(identificador, nome, email);
