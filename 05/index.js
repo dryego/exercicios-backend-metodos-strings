b1c3211d64d4756b6b981795a00ff42898449915
dryego barbosa <dryego.lisboa@gmail.com>
2022-10-07 10:43:30 -0300

    Resolução, mascarar numero de cartão.

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,11 @@
 const numeroCartao = '1111222233334444';
+
+if (numeroCartao.length === 16) {
+
+    //numeroCartao.replace(/\D/g, " ");
+    const ocultarCartao = numeroCartao.replace(/(\d{4})(\d{4})(\d{4})(\d{4})/, "$1********$4")
+
+    console.log(ocultarCartao);
+} else {
+    console.log("Numero cartão invalido.");
+}
