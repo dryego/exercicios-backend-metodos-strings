f3b898f97ebaa71a1665d244def5c8893b388f6d
dryego barbosa <dryego.lisboa@gmail.com>
2022-10-08 15:34:11 -0300

    resolução, funcao para verificar email.

--- 07/index.js
+++ 07/index.js
@@ -1,14 +1,18 @@
 const email = "aluno@cubos.academy";
 
-const verificadorArroba = email.indexOf("@");
-const verificadorPonto = email.indexOf(".", verificadorArroba);
-const validar = email.includes("@")
-const inicio = email.slice(0, 1);
-const finalEmail = email.slice(email.length - 1);
+function validarEmail(email) {
+    const verificadorArroba = email.indexOf("@");
+    const verificadorPonto = email.indexOf(".", verificadorArroba);
+    const validar = email.includes("@")
+    const inicio = email.slice(0, 1);
+    const finalEmail = email.slice(email.length - 1);
 
-if (validar === true && verificadorPonto > verificadorArroba && inicio != "." && finalEmail != "." && email.length > 3) {
+    if (validar === true && verificadorPonto > verificadorArroba && inicio != "." && finalEmail != "." && email.length > 3) {
 
-    console.log("E-mail valido.")
-} else {
-    console.log("E-mail inválido.");
+        console.log("E-mail valido.")
+    } else {
+        console.log("E-mail inválido.");
+    }
 }
+
+validarEmail(email);
