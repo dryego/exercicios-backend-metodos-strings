ecd533037a36c91dad61b9e14d48f43b200ad4c3
dryego barbosa <dryego.lisboa@gmail.com>
2022-10-08 16:16:55 -0300

    Resolução, remover caracter CPF/CNPJ.

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,11 @@
-const cpf = "011.022.033-44";
+const cnpj = "37.876.501/0001-46";
+const cpf = "011.022.033-44"
+
+function removerCaracter(cpf, cnpj) {
+    const dados = cnpj || cpf;
+    const Formatado = dados.replace(/[^0-9]/g, "");
+    return Formatado;
+}
+
+console.log(removerCaracter(cnpj));
+console.log(removerCaracter(cpf));
