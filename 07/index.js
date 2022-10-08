3798fd48efe2b9e70cbb1a187aaa9ee7a2e3c6bc
dryego barbosa <dryego.lisboa@gmail.com>
2022-10-08 15:31:05 -0300

    Resolução, validar email.

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,14 @@
 const email = "aluno@cubos.academy";
+
+const verificadorArroba = email.indexOf("@");
+const verificadorPonto = email.indexOf(".", verificadorArroba);
+const validar = email.includes("@")
+const inicio = email.slice(0, 1);
+const finalEmail = email.slice(email.length - 1);
+
+if (validar === true && verificadorPonto > verificadorArroba && inicio != "." && finalEmail != "." && email.length > 3) {
+
+    console.log("E-mail valido.")
+} else {
+    console.log("E-mail inválido.");
+}
