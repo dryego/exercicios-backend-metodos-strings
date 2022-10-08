02398209d298f7c3e19f5c0f65e16878144e91c6
dryego barbosa <dryego.lisboa@gmail.com>
2022-10-08 16:43:31 -0300

    Resolução, funcão para formatar nickName.

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,16 @@
 const nome = 'Guido Cerqueira';
+
+function nicknome(nome) {
+    const nomeMinusculo = nome.toLowerCase();
+    const nomeFormatado = nomeMinusculo.replace(/[^a-z]/g, "")
+    if (nome.length <= 13) {
+
+        const nicknome = (`@${nomeFormatado}`);
+
+        return nicknome;
+    } else {
+        return "Nickname invalido"
+    }
+}
+
+console.log(nicknome(nome));
